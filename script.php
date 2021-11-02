de00e7e65b23dd02fd48dccdc0c20c5d23a7ce7d
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-11-02 15:48:06 -0400

    added styles to the page

--- script.php
+++ script.php
@@ -1,27 +1,29 @@
-<?php 
-  $host = "localhost";
-  $user = "root";
-  $pass = "";
-  $db = "form";
+<?php include $_SERVER["DOCUMENT_ROOT"] . "./styles.scss";
 
-  $con = new mysqli($host,$user,$pass,$db);
-  if(!$con){
-    echo "There are some problems while connecting the database";
-  }
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "form";
 
-  $firstname = $_POST['firstname'];
-  $lastname = $_POST['lastname'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
-  $address = $_POST['address'];
+$con = new mysqli($host,$user,$pass,$db);
+if(!$con){
+echo "There are some problems while connecting the database";
+}
 
-  $query = "INSERT INTO `Black_Tech_Form`(`first_name`, `last_name`, `email`, `phone`, `address`) VALUES ('$firstname','$lastname','$email',$phone,'$address')";
-  
-  $insert = mysqli_query($con, $query);
-  
-  if(!$insert){
-    echo "There are some problems while inserting data";
-  } else {
-      echo "Form submitted successfully.  Thanks you buddy! We will get in touch.";
-  }
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+
+$query = "INSERT INTO `Black_Tech_Form`(`first_name`, `last_name`, `email`, `phone`, `address`) VALUES
+('$firstname','$lastname','$email',$phone,'$address')";
+
+$insert = mysqli_query($con, $query);
+
+if(!$insert){
+echo "There are some problems while inserting data";
+} else {
+echo "<body style='background:#060b23; color: white;'><div style='display:flex; justify-content:center; align-item:center; font-size:1.5rem; margin: 4rem 0 0 0'>Form submitted successfully. Thanks buddy! We will get in touch.</div></body>";
+}
 ?>
